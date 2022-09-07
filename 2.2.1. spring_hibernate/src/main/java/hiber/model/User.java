0a2715a8b9e0544081ffedc81a83ff381937b2a0
Ruslan <ruslanzel@list.ru>
2022-09-07 15:05:25 +0300

    На проверку

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -6,56 +6,88 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-   @Column(name = "name")
-   private String firstName;
+    @Column(name = "name")
+    private String firstName;
 
-   @Column(name = "last_name")
-   private String lastName;
+    @Column(name = "last_name")
+    private String lastName;
 
-   @Column(name = "email")
-   private String email;
+    @Column(name = "email")
+    private String email;
 
-   public User() {}
-   
-   public User(String firstName, String lastName, String email) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.email = email;
-   }
+    // Создаем зависимость @OneToOne по id
 
-   public Long getId() {
-      return id;
-   }
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cars_id")
+    private Car cars_id;
 
-   public void setId(Long id) {
-      this.id = id;
-   }
+    public User() {
+    }
 
-   public String getFirstName() {
-      return firstName;
-   }
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
 
-   public String getLastName() {
-      return lastName;
-   }
+    public User(String firstName, String lastName, String email, Car cars_id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.cars_id = cars_id;
+    }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
+    public Car getСars_id() {
+        return cars_id;
+    }
 
-   public String getEmail() {
-      return email;
-   }
+    public void setСars_id(Car cars_id) {
+        this.cars_id = cars_id;
+    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User id - " + id +
+                ", Имя - " + firstName +
+                ", Фамилия - " + lastName +
+                ", email - " + email +
+                ", Авто: " + cars_id;
+    }
 }
